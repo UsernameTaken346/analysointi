b4764cf6059665ac4b5a6a45871879107a011828
M-Lyly <mirzikatajalyly@gmail.com>
2023-11-04 18:31:07 +0200

    Update list-exercise.py

--- list-exercise.py
+++ list-exercise.py
@@ -17,3 +17,11 @@ thrownDiceNumbers = []
 
 for _ in range(5):
     thrownDiceNumbers.append(randint(1, 6))
+
+print("Whole list:", thrownDiceNumbers)
+
+list_sum = sum(thrownDiceNumbers)
+print("Sum of list values:", list_sum)
+
+highest_value = max(thrownDiceNumbers)
+print("Highest value in the list:", highest_value)
