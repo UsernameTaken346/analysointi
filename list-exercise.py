a5cbb81d4ecc34796522ae8aec20aeae85da4faa
M-Lyly <mirzikatajalyly@gmail.com>
2023-11-04 18:29:25 +0200

    Update list-exercise.py

--- list-exercise.py
+++ list-exercise.py
@@ -1,3 +1,10 @@
 furniture_list = ["table", "chair", "shelf", "sofa", "bed"]
 
 print("Whole list:", furniture_list)
+
+print("First two elements:", furniture_list[:2])
+
+for item in furniture_list:
+    if item == "sofa":
+        print("Sofa is in the list.")
+        break
