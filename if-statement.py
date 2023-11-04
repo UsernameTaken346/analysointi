53de315c3f6f3b4a555f42278193ea13806db183
M-Lyly <mirzikatajalyly@gmail.com>
2023-11-04 18:16:42 +0200

    Update if-statement.py

--- if-statement.py
+++ if-statement.py
@@ -3,17 +3,25 @@ number2 = 7
 number3 = 3
 
 
-if number1 == number2:
-    print("number1 and number2 are equal")
+if number1 == number2 == number3:
+    print("All numbers are equal")
+
+
+if (number1 == number2) or (number2 == number3):
+    print("number1 and number2 are equal, or number2 and number3 are equal")
+
+
+if number1 > number2 and number1 > number3:
+    print("number1 is greater than number2 and number3")
 
 
 if number1 > number2:
     print("number1 is greater than number2")
+elif number2 > number3:
+    print("number2 is greater than number3")
 
 
-if number1 >= number2:
-    print("number1 is greater or equal to number2")
-
-
-if number1 != number2:
-    print("number1 and number2 are not equal")
+if number1 == number2:
+    print("number1 and number2 are equal")
+elif number1 == number3:
+    print("number1 and number3 are equal")
