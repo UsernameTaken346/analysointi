b57b21d3764755d820c2e43cf29cb68448845448
M-Lyly <mirzikatajalyly@gmail.com>
2023-11-04 18:15:02 +0200

    Create if-statement.py

---
+++ if-statement.py
@@ -0,0 +1,18 @@
+number1 = 5
+number2 = 7
+
+
+if number1 == number2:
+    print("number1 and number2 are equal")
+
+
+if number1 > number2:
+    print("number1 is greater than number2")
+
+
+if number1 >= number2:
+    print("number1 is greater or equal to number2")
+
+
+if number1 != number2:
+    print("number1 and number2 are not equal")
