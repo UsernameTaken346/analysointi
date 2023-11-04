05672a5e484a7d0ea3e329c893eda14878fdc78e
M-Lyly <mirzikatajalyly@gmail.com>
2023-11-04 18:22:05 +0200

    Update if-statement.py

--- if-statement.py
+++ if-statement.py
@@ -1,4 +1,4 @@
-number1 = 5
+number1 = 8
 number2 = 7
 number3 = 3
 
@@ -20,8 +20,24 @@ if number1 > number2:
 elif number2 > number3:
     print("number2 is greater than number3")
 
-
 if number1 == number2:
     print("number1 and number2 are equal")
 elif number1 == number3:
     print("number1 and number3 are equal")
+
+name1 = "Alice"
+name2 = "Bob"
+name3 = "Alice"
+
+if name1 == name2:
+    print("name1 and name2 are equal")
+
+
+if name1 != name2:
+    print("name1 and name2 are not equal")
+
+
+if name1 == name2:
+    print("name1 and name2 are equal")
+elif name1 == name3:
+    print("name1 and name3 are equal")
